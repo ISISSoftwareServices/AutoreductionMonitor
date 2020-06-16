012fe13b7eb00cdb52aec595dd32f598bdb80c24
JackEAllen <mr.jack.e.allen@gmail.com>
2020-06-16 08:27:30 +0100

    Creation of dockerfile, documentation  detailing mysql db creation and db schema file

---
+++ database_containerisation/README.md
@@ -0,0 +1,38 @@
+# Containerised Database with Persistent Storage Mounted to External Data Volumes
+
+Why choose this architecture?
+
+Compartmentalises a database to allow for easy updates to the database and even easier migrations.
+
+The data is stored separately from the database in a data volume. Data volumes can be located on the 
+same host machine somewhere else over a network.
+
+Use the steps detailed below to access/modify the image, container or database:
+  
+### Build Docker Image with MySQL_image_schema.sql File Located in Current Directory
+* `docker build -t system_monitoring_schema .`
+
+### Run The Container with Attached Data Volume and Image Containing Schema from DockerFile
+* `docker run -p 15333:1433 --volumes-from mysqldata --name system_monitoring_db -d jack/mysql_system_monitoring`
+
+### Go Inside Container
+* `docker exec -it testcontainer3 /bin/bash`
+
+### Sign in using credentials
+* `mysql -uadmin -padmin`
+
+### View Database and Tables of Interest
+* `show databases;`
+* `use system_monitoring;`
+* `show tables;` - want to see tables matching schema so e.g. dates; instrument; statistics
+
+### Additional Instructions for Testing Purposes
+* Add data to each tables
+* Exit database and container:
+* Kill system_monitoring_db container:
+* Remove container:
+* Create container again using volume
+* Check new data added previously still exists in system_monitoring_db container database.
+
+
+ 
---
+++ database_containerisation/system_monitoring.sql
@@ -0,0 +1,85 @@
+-- MySQL dump 10.13  Distrib 8.0.18, for macos10.14 (x86_64)
+--
+-- Host: 127.0.0.1    Database: system_monitoring
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `dates`
+--
+
+DROP TABLE IF EXISTS `dates`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dates` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `instrument`
+--
+
+DROP TABLE IF EXISTS `instrument`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `instrument` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `instrument_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `statistics_counts`
+--
+
+DROP TABLE IF EXISTS `statistics_counts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `statistics_counts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `total_jobs` decimal(9,2) DEFAULT NULL,
+  `passed_jobs` decimal(9,2) DEFAULT NULL,
+  `failed_jobs` decimal(9,2) DEFAULT NULL,
+  `total_runs` decimal(9,2) DEFAULT NULL,
+  `passed_runs` decimal(9,2) DEFAULT NULL,
+  `failed_runs` decimal(9,2) DEFAULT NULL,
+  `average_execution_time` datetime DEFAULT NULL,
+  `started_by_ar` decimal(9,2) DEFAULT NULL,
+  `started_by_dev` decimal(9,2) DEFAULT NULL,
+  `started_by_user` decimal(9,2) DEFAULT NULL,
+  `resolved_runs` decimal(9,2) DEFAULT NULL,
+  `instrument_id` int DEFAULT NULL,
+  `date_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `instrument_id` (`instrument_id`),
+  KEY `date_id` (`date_id`),
+  CONSTRAINT `statistics_counts_ibfk_1` FOREIGN KEY (`instrument_id`) REFERENCES `instrument` (`id`),
+  CONSTRAINT `statistics_counts_ibfk_2` FOREIGN KEY (`date_id`) REFERENCES `dates` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-06-08 11:44:55
